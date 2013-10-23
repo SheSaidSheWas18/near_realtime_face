885b1c35c69da6f75034af96be9d639d9d9ce491
digvijay88 <ssr.digvijay88@gmail.com>
2013-10-23 16:21:24 +0530

    writing face_det.m

--- main_code/face_det.m
+++ main_code/face_det.m
@@ -3,15 +3,76 @@
 % random images, there is no need of tracking.
 
 function face_det(global_trflag)
+
+%Load images
+imgs = dir('data_dir/*jpg');
+
+%Load model
+load dramanan_files/face_p146_small.mat;
+model.interval = 5;
+model.thresh = min(-1, model.thresh);
+thresh = model.thresh;
+
+% define the mapping from view-specific mixture id to viewpoint
+if length(model.components)==13
+    posemap = 90:-15:-90;
+elseif length(model.components)==18
+    posemap = [90:-15:15 0 0 0 0 0 0 -15:-15:-90];
+else
+    error('Can not recognize this model');
+end
+
+%Make a strucuture for previous detections that can be used for tracking
+
+
+for i=1:numel(imgs)
+    %Load image
+    im_path = ['data_dir/' imgs(i).name];
+    im = imread(im_path);
     
-    %Load images
-    imgs = dir('data_dir/*jpg');
+    %Get viola-jones bounding box
+    [n det] = runFacedet(im_path,save_flag);
     
-    %Load model
-    load dramanan_files/face_p146_small.mat;
+    % No detection
+    if n == 0
+        %empty the previous detection strucuture because no tracking.
+        continue;
+    end;
     
-    for i=1:numel(imgs)
+    %For all the viola-jones detections, check if there is any overlap
+    %with the previous detection. If not there is no tracking.
+    
+    for ii = 1:n
+        
+        if global_trflag == 1 && i ~= 1
+            
+            %Checking for overlap between the current vj detection and tracked.
+            [check track_num] = check_overlap([det(ii,1),det(ii,3),det(ii,2),det(ii,4)],track_prev);
+            
+            if check == -1
+                %Pass empty set because no tracking
+            else
+                
+                %Tracking is there.
+            end;
+            
+            
+        else
+            %Pass empty set to Face_detect.
+        end;
+        
+        
+        % Face_detect part
+        ww = det(ii,2) - det(ii,1) + 1;
+        hh = det(ii,4) - det(ii,3) + 1;
+        face_bb = [1,size(im,2),1,size(im,1)];
+        face_bb(1) = max(face_bb(1),(round(det(ii,1)-0.3*ww)));
+        face_bb(2) = min(face_bb(2),(round(det(ii,2)+0.3*ww)));
+        face_bb(3) = max(face_bb(3),(round(det(ii,3)-0.3*hh)));
+        face_bb(4) = min(face_bb(4),(round(det(ii,4)+0.3*hh)));
+        
+        
+        
         
     end;
-
-end
+end;
